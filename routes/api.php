8ade939d2495519ba7f79c3c0269691d5a74161c
mhd-momtaz-alallaf <141574189+mhd-momtaz-alallaf@users.noreply.github.com>
2024-05-04 22:36:11 +0300

    handling the resource routes in the api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Users
+ */
+Route::resource('users',UserController::class)->except(['create','edit']);
+/**
+ * Buyers
+ */
+Route::resource('buyers',BuyerController::class)->only(['index','show']);
+/**
+ * Sellers
+ */
+Route::resource('sellers',SellerController::class)->only(['index','show']);
+/**
+ * Transactions
+ */
+Route::resource('transactions',TransactionController::class)->only(['index','show']);
+/**
+ * Products
+ */
+Route::resource('products',ProductController::class)->only(['index','show']);
+/**
+ * Categories
+ */
+Route::resource('categories',CategoryController::class)->except(['create','edit']);
+
