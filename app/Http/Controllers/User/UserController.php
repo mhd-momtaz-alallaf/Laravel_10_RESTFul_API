e2287bc47837a5969eee38651f40a6f1e9ebd28c
mhd-momtaz-alallaf <141574189+mhd-momtaz-alallaf@users.noreply.github.com>
2024-05-05 21:40:51 +0300

    implement the update method in the UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -53,10 +53,53 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id, // to except this user email to not be unique, in case if he dont want to change his email. 
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+        ];
+
+        $this->validate($request, $rules);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email') && $user->email != $request->email) { // if the user want to change his email, he will have to verifiy the new email
+            $user->verified = User::UNVERIFIED_USER; // mark the user as UNVERIFIED_USER.
+            $user->verification_token = User::generateVerificationCode(); // generate a new verification code.
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->has('admin')) {
+            if (!$user->isVerified()) {
+                return response()->json([
+                'error' => 'Only verified users can modify the admin field',
+                'code' => 409
+                ], 409); // 409 Conflict code
+            }
+
+            $user->admin = $request->admin;
+        }
+
+        if (!$user->isDirty()) { // ->isDirty() means the user has changed (one argument at least in the user model has changed).
+            return response()->json([
+                'error' => 'You need to specify a different value to update', 
+                'code' => 422
+            ], 422); // 422 Unprocessable Content
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
     }
+    
 
     /**
      * Remove the specified resource from storage.
