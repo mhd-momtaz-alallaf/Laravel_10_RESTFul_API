2d3ae71a61ec582e92e763cd514804c37a401872
mhd-momtaz-alallaf <141574189+mhd-momtaz-alallaf@users.noreply.github.com>
2024-05-08 23:08:02 +0300

    create and Implement the Index method for BuyerTransactionController

---
+++ app/Http/Controllers/Buyer/BuyerTransactionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerTransactionController extends ApiController
+{
+    public function index(Buyer $buyer)
+    {
+        $transactions = $buyer->transactions;
+
+        return $this->showAll($transactions);
+    }
+}
