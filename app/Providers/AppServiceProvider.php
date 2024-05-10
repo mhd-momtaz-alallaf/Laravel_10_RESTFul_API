79cba82f4997eaf631d831cc1753a97b5a292bc0
mhd-momtaz-alallaf <141574189+mhd-momtaz-alallaf@users.noreply.github.com>
2024-05-10 17:33:26 +0300

    handling the products availability using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Product::updated(function($product) { // to automaticly change the status of the product to UNAVAILABLE_PRODUCT if its became a 0.
+            if ($product->quantity == 0 && $product->isAvailable()) {
+                $product->status = Product::UNAVAILABLE_PRODUCT;
+
+                $product->save();
+            }
+        });
     }
 }
